23d093474c5be5a9cbf9c41f8b627fe2256fd420
REDWARS047 <redwars047@gmail.com>
2021-02-28 17:08:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/genota_imapp/Pm_Screen.java
@@ -0,0 +1,14 @@
+package com.example.genota_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pm_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pm__screen);
+    }
+}
---
+++ app/src/main/java/com/example/genota_imapp/Register_Screen.java
@@ -0,0 +1,14 @@
+package com.example.genota_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__screen);
+    }
+}
